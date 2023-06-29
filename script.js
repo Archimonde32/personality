f52ebd454fe887254feea1bae773d7d417d358bf
Archimonde32 <dkgaming718@gmail.com>
2023-06-29 16:25:17 +0200

    2

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Votre code Javascript
+function turnblackwhite(){
+
+}
+// Add an event listener to the images with the class "accordion-image"
+var images = document.getElementsByClassName("accordion-image");
+for (var i = 0; i < images.length; i++) {
+images[i].addEventListener("click", toggleImageColor);
+}
+
+// Toggle the image between black and white and color
+function toggleImageColor(event) {
+var bwDiv = event.currentTarget.querySelector(".bw");
+var colorDiv = event.currentTarget.querySelector(".color");
+
+  // Toggle the visibility of the black and white and color divs
+if (bwDiv.style.display === "none") {
+    bwDiv.style.display = "block";
+    colorDiv.style.display = "none";
+} else {
+    bwDiv.style.display = "none";
+    colorDiv.style.display = "block";
+}
+}
